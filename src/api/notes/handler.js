2ea6e002afbd0baa4285cd04b1b60324e9aced61
patriotadev <patriota.alba12@gmail.com>
2022-04-28 22:55:05 +0700

    authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -4,21 +4,23 @@ class NotesHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
- 
+
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
     this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
     this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
- 
+
   async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
- 
-      const noteId = await this._service.addNote({ title, body, tags });
- 
+      const { id: credentialId } = request.auth.credentials;
+      const noteId = await this._service.addNote({
+        title, body, tags, owner: credentialId,
+      });
+
       const response = h.response({
         status: 'success',
         message: 'Catatan berhasil ditambahkan',
@@ -37,7 +39,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -48,9 +50,10 @@ class NotesHandler {
       return response;
     }
   }
- 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: 'success',
       data: {
@@ -58,10 +61,12 @@ class NotesHandler {
       },
     };
   }
- 
+
   async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -78,7 +83,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -89,15 +94,15 @@ class NotesHandler {
       return response;
     }
   }
- 
+
   async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
-      const { title, body, tags } = request.payload;
       const { id } = request.params;
- 
-      await this._service.editNoteById(id, { title, body, tags });
- 
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.editNoteById(id, request.payload);
+
       return {
         status: 'success',
         message: 'Catatan berhasil diperbarui',
@@ -111,7 +116,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -122,12 +127,14 @@ class NotesHandler {
       return response;
     }
   }
- 
+
   async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.deleteNoteById(id);
- 
+
       return {
         status: 'success',
         message: 'Catatan berhasil dihapus',
@@ -141,7 +148,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -153,5 +160,5 @@ class NotesHandler {
     }
   }
 }
- 
+
 module.exports = NotesHandler;
--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,4 @@
-const { nanoid } = require('nanoid')
+const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
 
@@ -7,18 +7,18 @@ class NotesService {
     this._notes = [];
   }
 
-  addNote({ title, body, tags}) {
+  addNote({ title, body, tags }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
     const newNote = {
       title, tags, body, id, createdAt, updatedAt,
     };
- 
-    this._notes.push(newNote);
-    const isSuccess = this._notes.filter((note) => note.id ===id).length > 0;
 
-    if(!isSuccess) {
+    this._notes.push(newNote);
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    if (!isSuccess) {
       throw new InvariantError('Catatan gagal ditambahkan');
     }
 
@@ -40,13 +40,13 @@ class NotesService {
 
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
- 
+
     if (index === -1) {
       throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
     }
- 
+
     const updatedAt = new Date().toISOString();
- 
+
     this._notes[index] = {
       ...this._notes[index],
       title,
@@ -63,7 +63,6 @@ class NotesService {
     }
     this._notes.splice(index, 1);
   }
-
 }
 
 module.exports = NotesService;
